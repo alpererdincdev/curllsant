6e7ebc548aaadce9e1c8be3c884fb22b668597d4
Ekin Varli <marshiquezdev@protonmail.com>
2022-03-28 08:15:08 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+
+/*
+
+EN
+
+This project is under construction. We will carry out the development process through this repo.
+
+TR
+
+Bu proje yapım aşamasındadır. Geliştirme sürecini bu repo üzerinden gerçekleştireceğiz.
+
+*/
+
+console.log('Hello World');
